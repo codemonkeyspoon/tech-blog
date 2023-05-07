c63f5b4438601a3d7a6cd95d68e3dba7db1ef3da
codemonkeyspoon <98781257+codemonkeyspoon@users.noreply.github.com>
2023-05-07 10:30:58 -0400

    created models

--- models/index.js
+++ models/index.js
@@ -1,13 +1,29 @@
+const Post = require('./Post');
 const User = require('./User');
-const Project = require('./Project');
+const Comment = require('./Comment');
 
-User.hasMany(Project, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
-});
-
-Project.belongsTo(User, {
+User.hasMany(Post, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Project };
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Comment };
